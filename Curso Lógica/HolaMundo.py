25deab056fd5aea42dda8100ffa3e9549d9c727b
sitesystem <l_aloaxa05@hotmail.com>
2022-04-09 10:18:37 -0500

    Ejemplo Curso Python

---
+++ Curso Lógica/HolaMundo.py
@@ -0,0 +1,8 @@
+# COMENTARIO: Sirven para documentar o especificar 
+# el código o programa y entederlo mejor
+# Los comentario los omite o no les hace caso python y no los ejecuta
+
+# Imprimir o mostrar mensajes en pantalla
+print(2+5)
+print("HOLA MUNDO")
+print('EDUARDO')
